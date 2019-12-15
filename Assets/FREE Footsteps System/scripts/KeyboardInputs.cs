cf0c5e0c225f147b2528a12c0acf1c14fff3b6e2
Luis Herres <luis@herres.dev>
2019-12-15 19:48:06 +0100

    made chracter transparent, added objects to shoes

--- Assets/FREE Footsteps System/scripts/KeyboardInputs.cs
+++ Assets/FREE Footsteps System/scripts/KeyboardInputs.cs
@@ -3,6 +3,20 @@ using UnityEngine.SceneManagement;
 
 public class KeyboardInputs : MonoBehaviour
 {
+    public Camera cam;
+    private int _mymask;
+
+    private void Start()
+    {
+        // Store a copy of your cullingmask
+        _mymask = cam.cullingMask;
+
+        // Only render objects in the first layer (Default layer)
+        //cam.cullingMask = 1 << 0;
+
+        // do something
+    }
+
     // Update is called once per frame
     void Update()
     {
