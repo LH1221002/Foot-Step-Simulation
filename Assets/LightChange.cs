0a95838ec99791c8323ccd7a268b1efda7b716bf
Luis <luis@herres.dev>
2020-03-06 17:03:17 +0100

    alignment stuff

--- Assets/LightChange.cs
+++ Assets/LightChange.cs
@@ -6,12 +6,18 @@ using UnityEngine;
 public class LightChange : MonoBehaviour
 {
     public Light light;
+    public GameObject shoe;
+    public GameObject s;
+    public GameObject cont;
 
     private ShoeController rightShoe;
+    
     // Start is called before the first frame update
     public IEnumerator Start()
     {
-
+        
+     
+        s.transform.localRotation= Quaternion.Euler(s.transform.localRotation.eulerAngles.x, cont.transform.localRotation.y, s.transform.localRotation.eulerAngles.z);
         yield return new WaitForSeconds(2);
         Debug.Log("Los gehts");
         rightShoe = GetComponent<ShoeController>();
@@ -22,6 +28,25 @@ public class LightChange : MonoBehaviour
         yield return new WaitForSeconds(2);
         light.color = Color.green;
         yield return new WaitForSeconds(5);
+        RaycastHit hit;
+        var forward = transform.TransformDirection(Vector3.up) * 10;
+        if (Physics.Raycast(transform.position, forward, out hit, Mathf.Infinity))
+        {
+            Debug.DrawRay(transform.position, forward, Color.yellow, 25);
+            Debug.Log("Did Hit");
+
+
+            var forwarsd = -hit.transform.gameObject.transform.up * 10;
+            var e = Quaternion.LookRotation(-hit.transform.up).eulerAngles;
+            shoe.transform.rotation = Quaternion.FromToRotation(-Vector3.up, hit.normal);
+            Debug.DrawRay(hit.transform.gameObject.transform.position, hit.normal * 10, Color.green, 25);
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, forward, Color.white, 25);
+            Debug.Log("Did not Hit");
+        }
+
         rightShoe.CalibrateMax();
 
         yield return new WaitForSeconds(3);
@@ -30,7 +55,8 @@ public class LightChange : MonoBehaviour
 
         rightShoe.ReceiveData(ChangeLight);
     }
-
+    
+    
     private void ChangeLight(int roh, int cal) {
         light.intensity = cal;
         //print(cal);
